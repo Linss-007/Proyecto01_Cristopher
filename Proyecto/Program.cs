70468fa81d47f5293b3ced1471dd553fa4db1f89
Linss-007 <cristopheradl7@gmail.com>
2026-03-15 16:13:11 -0600

    Creación de menu y switch para opciones

--- Proyecto/Program.cs
+++ Proyecto/Program.cs
@@ -1,1 +1,46 @@
-﻿
+﻿int opcion;
+do
+{
+    menu();
+    switch (opcion)
+    {
+        case 1:
+
+        break;
+
+        case 2:
+
+        break;
+
+        case 3:
+
+        break;
+
+        case 4:
+
+        break;
+
+        case 5:
+            Console.WriteLine("Saliendo...");
+        break;
+
+        default:
+            Console.WriteLine("Opción no válida");
+        break;
+    }
+}while (opcion != 5);
+
+
+
+
+void menu()
+{
+    Console.WriteLine("Bienvenido al menu de streaming");
+    Console.WriteLine("1. Evaluar contenido");
+    Console.WriteLine("2. Mostrar reglas");
+    Console.WriteLine("3. Mostrar estadísticas de la sesión");
+    Console.WriteLine("4. Reiniciar estadísticas");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("Elija una opción para continuar");
+    opcion = int.Parse(Console.ReadLine());
+}
